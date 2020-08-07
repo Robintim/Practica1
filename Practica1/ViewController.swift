a0155920494428f180d5d1c232dc93ddbe9d15e8
Daniel Alberto Vázquez Rodríguez <bpxrs1999@yahoo.com.mx>
2020-08-07 17:26:58 -0500

    Initial Commit

---
+++ Practica1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica1
+//
+//  Created by Daniel Alberto Vazquez Rodriguez on 8/7/20.
+//  Copyright © 2020 Daniel Alberto Vazquez Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
